dd9a3c82c5510af09bc5d146d0ebcdae1fa37380
MiSungKim <pinklike23@gmail.com>
2021-02-26 02:23:27 +0900

    [UPDATE]오류 수정 및 footer디자인 수정

--- src/components/atoms/MainAboutEcVideo.js
+++ src/components/atoms/MainAboutEcVideo.js
@@ -34,9 +34,9 @@ function AboutEcVideo() {
         width="100%"
         height="100%"
         src="https://www.youtube.com/embed/6QosveEmeqg"
-        frameborder="0"
+        frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
+        allowFullScreen
       />
     </VideoBlock>
     </div>
